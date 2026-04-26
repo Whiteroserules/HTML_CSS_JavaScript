1b4921465bd7f7cde17d85036f7ff9c5a68b1a28
Whiteroserules <whiteroserules12@gmail.com>
2026-04-26 13:57:40 -0400

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,31 @@
+function validate(e) {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('password').value;
+    const age = document.getElementById('age').value;
+    const msgbox = document.getElementById('message');
+
+    let message = '';
+    if (email === '') {
+        message = "please enter an email";
+        msgbox.style.color = 'red';
+    } else if (pass === '') {
+        message = "please enter a password";
+        msgbox.style.color = 'red';
+    }  else if (age === '') {
+        message = "please enter your age";
+        msgbox.style.color = 'red';
+    } else {
+        message = "login Succesful";
+        msgbox.style.color = 'green';
+    }
+    msgbox.innerHTML = message;
+
+
+}
+
+document.getElementById("loginForm").onsubmit = validate;
+
+document.getElementById("email").oninput = () => validate({preventDefault: () => {} });
+document.getElementById("password").oninput = () => validate({preventDefault: () => {} });
+document.getElementById("age").oninput = () => validate({preventDefault: () => {} });
